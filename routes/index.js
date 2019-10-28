7562d0ce70123c5f2ada2b4a967068dfe3055c9e
Henry Han <potatoeater0127@gmail.com>
2019-10-28 14:13:14 +0800

    (feat) Added admin restaurants index page

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,12 @@
 const restController = require('../controllers/restController.js');
+const adminConroller = require('../controllers/adminController.js');
 module.exports = app => {
   app.get('/', (req, res) => {
     res.redirect('/restaurants');
   });
   app.get('/restaurants', restController.getRestaurants);
+  app.get('/admin', (req, res) => {
+    res.redirect('/admin/restaurants');
+  });
+  app.get('/admin/restaurants', adminConroller.getRestaurants);
 };
