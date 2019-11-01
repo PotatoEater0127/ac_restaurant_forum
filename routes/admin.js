f750aa57b3342fb005e685de28950bb1cbaa5ed2
Henry Han <potatoeater0127@gmail.com>
2019-11-01 11:47:48 +0800

    (refactor) Extract /admin route

---
+++ routes/admin.js
@@ -0,0 +1,25 @@
+const adminControllor = require('../controllers/adminController.js');
+const admin = require('express').Router();
+
+const authenticatedAdmin = (req, res, next) => {
+  if (req.isAuthenticated()) {
+    if (req.user.isAdmin) {
+      return next();
+    }
+    return res.redirect('/');
+  }
+  res.redirect('/signin');
+};
+
+admin.use(authenticatedAdmin);
+admin.get('/', (req, res) => {
+  res.redirect('/restaurants');
+});
+admin.route('/restaurants')
+      .get(adminControllor.getRestaurants)
+      .post(adminControllor.postRestaurant)
+admin.get('/restaurants/create', adminControllor.createRestaurant);
+admin.get('/restaurants/:id', adminControllor.getRestaurant);
+
+
+module.exports = admin;
--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,12 @@
 const restController = require('../controllers/restController.js');
 const adminConroller = require('../controllers/adminController.js');
 const userController = require('../controllers/userController.js');
+const admin = require('./admin.js');
 
 module.exports = (app, passport) => {
   const authenticated = (req, res, next) => {
     if (req.isAuthenticated()) {
       return next();
-    }
-    res.redirect('/signin');
-  };
-  const authenticatedAdmin = (req, res, next) => {
-    if (req.isAuthenticated()) {
-      if (req.user.isAdmin) {
-        return next();
-      }
-      return res.redirect('/');
     }
     res.redirect('/signin');
   };
@@ -26,43 +18,21 @@ module.exports = (app, passport) => {
   app.get('/restaurants', authenticated, restController.getRestaurants);
 
   //* requests of admin
-  app.get('/admin', authenticatedAdmin, (req, res) => {
-    res.redirect('/admin/restaurants');
-  });
-  app.get(
-    '/admin/restaurants/:id',
-    authenticatedAdmin,
-    adminConroller.getRestaurant,
-  );
-  app.get(
-    '/admin/restaurants',
-    authenticatedAdmin,
-    adminConroller.getRestaurants,
-  );
-  app.get(
-    '/admin/restaurants/create',
-    authenticatedAdmin,
-    adminConroller.createRestaurant,
-  );
-  app.post(
-    '/admin/restaurants',
-    authenticatedAdmin,
-    adminConroller.postRestaurant,
-  );
-  //*
+  app.use('/admin', admin);
 
   //* requests of signup
-  app.get('/signup', userController.signUpPage);
-  app.post('/signup', userController.signUp);
+  app.route('/signup')
+    .get(userController.signUpPage)
+    .post(userController.signUp);
 
   //* requests of signin
-  app.get('/signin', userController.signInpage);
-  app.post(
-    '/signin',
-    passport.authenticate('local', {
-      failureRedirect: 'signin',
-      failureFlash: true,
-    }),
-    userController.signIn,
-  );
+  app.route('/signin')
+    .get(userController.signInpage)
+    .post(
+      passport.authenticate('local', {
+        failureRedirect: 'signin',
+        failureFlash: true,
+      }),
+      userController.signIn,
+    );
 };
