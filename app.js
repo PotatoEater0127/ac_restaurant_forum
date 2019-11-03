e84e1e1a9450bda5081268d833513f736155138d
Henry Han <potatoeater0127@gmail.com>
2019-11-03 19:44:15 +0800

    (clean) Clean up some legacy code

--- app.js
+++ app.js
@@ -6,27 +6,18 @@ const session = require("express-session");
 const methodOverride = require("method-override");
 const passport = require("./config/passport");
 const flashHelper = require("./middlewares/flashHelper.js");
-
 const db = require("./models");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-//* set up view engine
 app.engine("handlebars", handlebars({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
-//* set up bodyParser
 app.use(bodyParser.urlencoded({ extended: true }));
-//* set up method override
 app.use(methodOverride("_method"));
-//* set up session and flash
 app.use(session({ secret: "secret", resave: false, saveUninitialized: false }));
-//* set up passport
-app.use(passport.initialize());
-app.use(passport.session());
-//* set up flash and flash-helper
+app.use(passport.initialize(), passport.session());
 app.use(flash(), flashHelper("success_messages", "error_messages"));
-
 app.use((req, res, next) => {
   res.locals.user = req.user;
   next();
