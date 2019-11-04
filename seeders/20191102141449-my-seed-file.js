1698096691dcecbd2d790f27dcd5b1bd865f1e08
Henry Han <potatoeater0127@gmail.com>
2019-11-04 16:39:27 +0800

    (Add) Add category seed data

--- seeders/20191102141449-my-seed-file.js
+++ seeders/20191102141449-my-seed-file.js
@@ -33,6 +33,25 @@ module.exports = {
       ],
       {}
     );
+    queryInterface.bulkInsert(
+      "Categories",
+      [
+        "中式料理",
+        "日本料理",
+        "義大利料理",
+        "墨西哥料理",
+        "素食料理",
+        "美式料理",
+        "複合式料理"
+      ].map((item, index) => ({
+        id: index + 1,
+        name: item,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      })),
+      {}
+    );
+
     return queryInterface.bulkInsert(
       "Restaurants",
       Array.from({ length: 50 }).map(() => ({
@@ -43,7 +62,8 @@ module.exports = {
         image: faker.image.imageUrl(),
         description: faker.lorem.text(),
         createdAt: new Date(),
-        updatedAt: new Date()
+        updatedAt: new Date(),
+        CategoryId: Math.floor(Math.random() * 5) + 1
       })),
       {}
     );
@@ -52,6 +72,7 @@ module.exports = {
   down: (queryInterface, Sequelize) => {
     return queryInterface
       .bulkDelete("Restaurants", null, {})
-      .then(() => queryInterface.bulkDelete("Users", null, {}));
+      .then(() => queryInterface.bulkDelete("Users", null, {}))
+      .then(() => queryInterface.bulkDelete("Categories"), null, {});
   }
 };
